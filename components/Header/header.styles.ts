e3f0585e7e800989c9269923befe1027d53cca0b
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2022-07-24 11:58:17 +0100

    feat: initial header

---
+++ components/Header/header.styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+export const CityInfo = styled.div`
+  padding: 12px;
+  h2 {
+    margin: 0;
+  }
+  p {
+    margin: 4px;
+  }
+`;
+
+export const Search = styled.div`
+  padding: 12px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: end;
+`;
---
+++ components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { CityInfo, Header, Search } from "./header.styles";
+
+const HeaderComponent = () => {
+  return (
+    <Header>
+      <CityInfo>
+        <h2>Porto</h2>
+        <p>Portugal</p>
+        <p className="date">Monday, 12 July 2022 08:00</p>
+      </CityInfo>
+
+      <Search>
+        <input type="text" placeholder="Enter yout location" />
+      </Search>
+    </Header>
+  );
+};
+
+export default HeaderComponent;
