4829d1586bf0c4e482a4b0c0059ffc7619d06931
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2022-07-24 11:45:12 +0100

    feat: created main container

---
+++ components/MainContainer/index.tsx
@@ -0,0 +1,7 @@
+import { Main } from "./mainContainer.styles";
+
+const MainContainer = ({ children }: { children: JSX.Element }) => {
+  return <Main>{children}</Main>;
+};
+
+export default MainContainer;
---
+++ components/MainContainer/mainContainer.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  color: white;
+  background-color: #4276c9;
+  min-height: 10vh;
+  padding: 4rem 0;
+  margin: 4rem 0;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 5px 5px 15px 5px #aeb6c2;
+`;
