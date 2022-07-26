35cbf255bb9bf5e80aa2aa1aa24ab461f3163864
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2022-07-26 23:37:34 +0100

    fix: render after useWeather context

--- components/FavoriteList/index.tsx
+++ components/FavoriteList/index.tsx
@@ -1,11 +1,13 @@
 import { useCity } from "../../hooks/useCity";
 import { useFavorite } from "../../hooks/useFavorite";
+import { useWeather } from "../../hooks/useWeather";
 import { CurrentCity } from "../../models/City";
 import * as S from "./favoriteList.styles";
 
 const FavoriteList = () => {
   const { favorites, toggleFavoriteItem } = useFavorite();
   const { setCurrentCity } = useCity();
+  const { currentWeather } = useWeather();
 
   const handleCurrentCity = (favorite: CurrentCity) => {
     setCurrentCity(favorite);
@@ -13,16 +15,21 @@ const FavoriteList = () => {
 
   return (
     <S.FavoritesContainer>
-      <ul>
-        {favorites.map((favorite) => (
-          <li key={favorite.value} onClick={() => handleCurrentCity(favorite)}>
-            {favorite.label}
-            <button onClick={() => toggleFavoriteItem(favorite)}>
-              Remove from list
-            </button>
-          </li>
-        ))}
-      </ul>
+      {currentWeather && (
+        <ul>
+          {favorites.map((favorite) => (
+            <li
+              key={favorite.value}
+              onClick={() => handleCurrentCity(favorite)}
+            >
+              {favorite.label}
+              <button onClick={() => toggleFavoriteItem(favorite)}>
+                Remove from list
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </S.FavoritesContainer>
   );
 };
